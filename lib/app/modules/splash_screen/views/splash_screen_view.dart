a4865fddb45d61729f830428293ec7ecece6aa70
Indra <iwahyu@student.telkomuniversity.ac.id>
2023-02-09 06:58:28 +0700

    added splash screen & route

---
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/splash_screen_controller.dart';
+
+class SplashScreenView extends GetView<SplashScreenController> {
+  const SplashScreenView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/img/splash-screen.png', fit: BoxFit.fill),
+          Image.asset('assets/logo/main-logo.png'),
+        ],
+      )
+    );
+  }
+}
