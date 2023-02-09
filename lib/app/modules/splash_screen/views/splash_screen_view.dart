7101e9d22e4387d3bfca63e65db0c7493cdf1d48
Azis202017 <azisa6980@gmail.com>
2023-02-09 08:32:16 +0700

    Change splash screen

--- lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -1,21 +1,57 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
 import '../controllers/splash_screen_controller.dart';
+import '../../../theme/color.dart';
+import '../../../theme/font.dart';
 
 class SplashScreenView extends GetView<SplashScreenController> {
   const SplashScreenView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset('assets/img/splash-screen.png', fit: BoxFit.fill),
-          Image.asset('assets/logo/main-logo.png'),
-        ],
-      )
+     
+      body: GetBuilder<SplashScreenController>(
+        init: SplashScreenController(),
+        initState: (_) {
+          // Timer.periodic(const Duration(seconds: 2), (_) {
+          //   Get.toNamed(Routes.HOME);
+          // });
+        },
+        builder: (_) {
+          return Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Image.asset(
+                  'assets/images/background.png',
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/logo.png',
+                      width: 283,
+                      height: 283,
+                    ),
+                    Text(
+                      'TriftMee',
+                      style: headline1Bold.copyWith(
+                        color: primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/app/theme/font.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle headline1Bold = GoogleFonts.inter(
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+);
